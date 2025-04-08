16db2a5e56f26561b5779fcb0c8386711628c07f
beqir <etumer31148@gmail.com>
2025-04-08 10:47:24 +0300

    Self trial project - React Tutorial - initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header () {
+    return (
+        <h1 id="header">React Investment Calculator</h1>
+    );
+}
---
+++ src/components/UserInput.jsx
@@ -0,0 +1,22 @@
+export default function UserInput() {
+  return (
+    <section id="user-input" className="input-group">
+      <label>
+        INITIAL INVESTMENT
+        <input />
+      </label>
+      <label >
+        ANNUAL INVESTMENT
+        <input />
+      </label>
+      <label>
+        EXPECTED RETURN
+        <input />
+      </label>
+      <label>
+        DURATION
+        <input />
+      </label>
+    </section>
+  );
+}
